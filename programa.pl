89bf51d84a892d52ac3f59402d40c73d698baaed
kiarafrontini <kfrontini@frba.utn.edu.ar>
2026-08-10 23:08:38 -0300

    parte B

--- programa.pl
+++ programa.pl
@@ -10,3 +10,26 @@ ganasDeJugar(beto, 0.8).
 ganasDeJugar(Jugador, 0.4) :-
     progreso(Jugador, Posicion, _),
     Posicion > 25.
+
+%parte B
+
+empatados(Jugador1, Jugador2):-
+progreso(Jugador1, _, Dinero),
+progreso(Jugador2, _, Dinero),
+Jugador1 \= Jugador2. 
+
+ventaja(Jugador1, Jugador2):-
+    progreso(Jugador1, _, Dinero1),
+    progreso(Jugador2, _, Dinero2),
+    Dinero1 >= Dinero2. 
+
+vaMejor(Jugador):-
+    progreso(Jugador, _, _), 
+    forall(progreso(OtroJugador,_,_), ventaja(Jugador, OtroJugador)).
+
+estanMotivados(Jugador):-
+    vaMejor(Jugador). 
+
+estanMotivados(Jugador):-
+    progreso(Jugador, _, _),
+    forall(ventaja(Jugador, OtroJugador), (ganasDeJugar(OtroJugador, Ganas), 0.5 > Ganas)).
